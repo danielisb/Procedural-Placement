70b4f5144352a04f47ab9971dd11aca020ab116b
Daniel <danlisb88@gmail.com>
2020-09-01 14:58:06 -0300

    Added instantiate random size and rotation with public access to edit it on Unity.

--- Assets/Scripts/builder.cs
+++ Assets/Scripts/builder.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class builder : MonoBehaviour {
 
+    [Header("Terrain")]
     public Vector3 terrainArea; // the terrain you wants to place random objects
+    
+    [Header("Prefab")]
     public GameObject prefab;
+
+    [Header("Clones")]
     public int amountClones; // how many objects you want to place inside the terrain area
+
+    [Header("Size Settings")]
+    public float minSize, maxSize;
+    
     private float xMinimum, xMaximum, zMinimum, yMinimum, zMaximum;
     
     private void Start() {
@@ -25,6 +34,7 @@ public class builder : MonoBehaviour {
             SetPrefab();
         }
     }
+
     private void SetPrefab() {
 
         GameObject objPos = new GameObject();
@@ -37,13 +47,27 @@ public class builder : MonoBehaviour {
         if (Physics.Raycast (getHeight, out hit, 100)) {
             
             prefab = Instantiate(prefab, hit.point, Quaternion.identity);
-            //prefab.transform.localScale = new Vector3(0.07414654f, 0.07414654f, 0.07414654f);
-            prefab.transform.position = new Vector3(prefab.transform.position.x,
-                                                    prefab.transform.position.y,
-                                                    prefab.transform.position.z);
-
+            SetRandomPosition();
+            SetRandomRotation();
+            SetRandomScale();
             prefab.transform.parent = this.transform;
-            Destroy(objPos);
-        }
+        }        
+        Destroy(objPos);
+    }
+
+    void SetRandomPosition() {
+
+        prefab.transform.position = new Vector3(prefab.transform.position.x, prefab.transform.position.y, prefab.transform.position.z);
+    }
+
+    void SetRandomRotation() {
+
+        prefab.transform.rotation = Quaternion.Euler(prefab.transform.rotation.x, (Random.Range(0.0f, 360.0f)), prefab.transform.rotation.x);
+    }
+
+    void SetRandomScale() {
+
+        var randomSize = Random.Range(minSize, maxSize);        
+        prefab.transform.localScale = new Vector3(randomSize, randomSize, randomSize);
     }
 }
